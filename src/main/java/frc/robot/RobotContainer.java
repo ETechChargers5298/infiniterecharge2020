c3e5d713c7d9c6e487fcdb97bb90c1cba21da574
pepetheDimomdFrg <oelsayed@etech-nyc.org>
2020-01-16 16:39:16 -0500

    RobotContainer Linked with DriveTrain

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,7 +12,9 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.Drive;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.DriveTrain;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
@@ -22,10 +24,17 @@ import edu.wpi.first.wpilibj2.command.Command;
  * (including subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
+
+  private final DriveTrain dtn = new DriveTrain();
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+
+  private final Drive drive = new Drive();
+
+  XboxController drivecontroller = new XboxController(1);
+  XboxController operatController = new XboxController(2);
 
 
 
