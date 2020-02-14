f72e5f6c81a952e0f73c0a06f112a9e0283d968d
spetrovic3 <spetrovic@etech-nyc.org>
2020-02-14 18:39:53 -0500

    basic Autonomous

--- src/main/java/frc/robot/commands/Autonomous/AutonomousDriveStraight.java
+++ src/main/java/frc/robot/commands/Autonomous/AutonomousDriveStraight.java
@@ -5,36 +5,55 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.commands;
+package frc.robot.commands.Autonomous;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveTrain;
+import edu.wpi.first.wpilibj.Timer;
 
-public class Autonomous extends CommandBase {
+
+public class AutonomousDriveStraight extends CommandBase {
   /**
-   * Creates a new Autonomous.
+   * Creates a new AutonomousDriveStraight.
    */
-  public Autonomous() {
-    // Use addRequirements() here to declare subsystem dependencies.
+  
+  private DriveTrain drive;
+  private double speed;
+  private double maxTime;
+  private double startTime;
+  private boolean finished;
+
+  public AutonomousDriveStraight(DriveTrain drive, double speed, double timer) {
+    this.drive = drive;
+    this.speed = speed;
+    this.maxTime = timer;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    startTime = Timer.getFPGATimestamp();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    drive.arcadeDrive(speed, 0.0);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    if(Timer.getFPGATimestamp() - startTime >= maxTime) {
+        finished = true;
+    }
+    
+    return finished;
   }
 }
